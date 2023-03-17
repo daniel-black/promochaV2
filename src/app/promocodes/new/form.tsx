9106ddcf6fa2aeed5f307f99685cfbfed20b3f4c
daniel-black <drblack651@gmail.com>
2023-03-17 02:14:51 -0600

    add discount inputs

--- src/app/promocodes/new/form.tsx
+++ src/app/promocodes/new/form.tsx
@@ -5,14 +5,33 @@ import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 export default function Form() {
   const [code, setCode] = useState<string>('');
   const [type, setType] = useState<Type>('amount');
+  const [amountDiscount, setAmountDiscount] = useState<number>(5);
+  const [percentDiscount, setPercentDiscount] = useState<number>(5);
+  const [maxDiscount, setMaxDiscount] = useState<number>(Infinity);
 
   const handleCodeChange = (e: ChangeEvent<HTMLInputElement>) => setCode(e.target.value.toUpperCase());
 
   return (
     <section className="rounded p-6 bg-neutral-100 w-full max-w-lg border border-neutral-200">
       <form className="space-y-6">
-        <TextInput name="code" value={code} onChange={handleCodeChange} />
-        <TypeToggle type={type} setType={setType} />
+        <TextInput
+          name="code"
+          value={code}
+          onChange={handleCodeChange}
+        />
+        <TypeToggle
+          type={type}
+          setType={setType}
+        />
+        <DiscountInput
+          type={type}
+          amountDiscount={amountDiscount}
+          percentDiscount={percentDiscount}
+          maxDiscount={maxDiscount}
+          setAmountDiscount={setAmountDiscount}
+          setPercentDiscount={setPercentDiscount}
+          setMaxDiscount={setMaxDiscount}
+        />
       </form>
     </section>
   );
@@ -33,7 +52,7 @@ function TextInput({ name, value, onChange }: TextInputProps) {
         onChange={onChange}
         type="text"
         id={name}
-        className="uppercase w-full rounded-md p-3 bg-white border border-neutral-200"
+        className="uppercase font-mono w-full rounded-md p-3 bg-white border border-neutral-200"
       />
     </FormSectionWrapper>
   );
@@ -57,7 +76,7 @@ function TypeToggle({ type, setType }: TypeToggleProps) {
 
   return (
     <FormSectionWrapper>
-      <Label name="Discount Type" />
+      <Label name="discount type" />
       <div className="flex gap-2">
         <button
           className={`w-full ${isAmount ? 'bg-green-200 border-green-300 text-green-700' : 'bg-neutral-200 text-neutral-600 border-neutral-300 hover:bg-neutral-300'} py-3 rounded-md border disabled:cursor-not-allowed transition-all duration-100`}
@@ -80,8 +99,80 @@ function TypeToggle({ type, setType }: TypeToggleProps) {
   );
 }
 
+type DiscountInputProps = {
+  type: Type;
+  amountDiscount: number;
+  percentDiscount: number;
+  maxDiscount: number;
+  setAmountDiscount: Dispatch<SetStateAction<number>>;
+  setPercentDiscount: Dispatch<SetStateAction<number>>;
+  setMaxDiscount: Dispatch<SetStateAction<number>>;
+};
+
+function DiscountInput(props: DiscountInputProps) {
+  const isAmount = props.type === 'amount';
+
+  const handleDiscountChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (isAmount) {
+      props.setAmountDiscount(e.target.valueAsNumber);
+    } else {
+      props.setPercentDiscount(e.target.valueAsNumber);
+    }
+  }
+
+  return (
+    <>
+      <FormSectionWrapper>
+        <Label name="discount" />
+        <div className="relative">
+          <span className="absolute h-full w-12 bg-neutral-50 rounded-l-md border text-neutral-400 flex justify-center items-center">{isAmount ? '$' : '%'}</span>
+          <input
+            type="number"
+            value={isAmount ? props.amountDiscount : props.percentDiscount}
+            min={0}
+            max={isAmount ? 1000 : 100}
+            onChange={handleDiscountChange}
+            id="discount"
+            className="w-full outline-none font-mono rounded-md p-3 pl-16 bg-white border border-neutral-200"
+          />
+        </div>
+      </FormSectionWrapper>
+      {!isAmount &&
+        <FormSectionWrapper>
+          <div className="flex justify-between items-center">
+            <Label name="max discount" />
+            {props.maxDiscount !== Infinity &&
+              <button
+                className="text-xs underline text-neutral-500"
+                onClick={e => {
+                  e.preventDefault();
+                  props.setMaxDiscount(Infinity);
+                }}
+              >
+                Remove Max
+              </button>
+            }
+          </div>
+          <div className="relative">
+            <span className="absolute h-full w-12 bg-neutral-50 rounded-l-md border text-neutral-400 flex justify-center items-center">$</span>
+            <input
+              type="number"
+              value={props.maxDiscount}
+              min={0}
+              max={1000}
+              onChange={e => props.setMaxDiscount(e.target.valueAsNumber)}
+              id="discount"
+              className="w-full outline-none font-mono rounded-md p-3 pl-16 bg-white border border-neutral-200"
+            />
+          </div>
+        </FormSectionWrapper>
+      }
+    </>
+  );
+}
+
 function Label({ name }: { name: string }) {
-  return <label htmlFor={name} className="block text-neutral-500 capitalize">{name}</label>;
+  return <label htmlFor={name.replaceAll(' ', '-')} className="block text-neutral-500 capitalize">{name}</label>;
 }
 
 function FormSectionWrapper({ children }: { children: React.ReactNode }) {
